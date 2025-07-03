c0f553cf00014da392cbce7a7734acf064830124
keoinn <keoinn@gmail.com>
2025-07-03 09:50:39 +0800

    A | Add download Script.

---
+++ download_db.bat
@@ -0,0 +1,1 @@
+wget --no-check-certificate "https://docs.google.com/uc?export=download&id=1xeU4Xp7B1WDLyldnw2ilIrMOUtDpxG9R" -O db.sqlite3
---
+++ download_db.sh
@@ -0,0 +1,2 @@
+rm -rf db.sqlite3
+wget --no-check-certificate "https://docs.google.com/uc?export=download&id=1xeU4Xp7B1WDLyldnw2ilIrMOUtDpxG9R" -O db.sqlite3
