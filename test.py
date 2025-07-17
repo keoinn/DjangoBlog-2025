d9d3c39e9ab3c33cb3f39711dd0ecb49d274fda1
keoinn <keoinn@gmail.com>
2025-07-17 11:45:27 +0800

    20250717

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 from modules.crawler.utils import parserTargetURL
 from modules.crawler.utils import run
+from posts.models import Post
 
-# print(parserTargetURL('board'))
-# print(parserTargetURL('board', board='Baseball'))
-# print(parserTargetURL('board',
-#                       board='Baseball',
-#                       pageNum='19621'))
-# print(parserTargetURL('article'))
-# print(parserTargetURL('article',
-#                   articleURL='/bbs/Baseball/M.1752411597.A.6CA.html'))
-
-from modules.crawler.utils import run
 data = run(board='Baseball', pageNum='19621')
+
+for article in data:
+    article_model = Post(
+        title=article.title,
+        slug=article.url,
+        content=article.content
+    )
+    article.save()
+
 print(data)
